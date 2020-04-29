6af6f208cd1d1245609d96ab7dd5fe9412581be9
Lucas Wong <lucaswong.ca@gmail.com>
2020-04-29 01:52:35 +0000

    Another array that evaluates to arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return `✅✅✅You got the green light!: ${actual} === ${expected}`;
+  } else {
+    return `❌❌❌Doesn't quite match!: ${actual} !== ${expected}`;
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
