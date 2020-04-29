b8734f57d875fe2080847e1214858eb51f10debb
Lucas Wong <lucaswong.ca@gmail.com>
2020-04-29 02:06:24 +0000

    changed to console.log instead of return

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,14 +13,14 @@ const eqArrays = function(actual, expected) {
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    return `✅✅✅You got the green light!: ${actual} === ${expected}`;
+    console.log(`✅✅✅You got the green light!: ${actual} === ${expected}`);
   } else {
-    return `❌❌❌Doesn't quite match!: ${actual} !== ${expected}`;
+    console.log(`❌❌❌Doesn't quite match!: ${actual} !== ${expected}`);
   }
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
