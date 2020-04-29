9a892e0005519c44a5704f42c842803f9e3aafe9
Lucas Wong <lucaswong.ca@gmail.com>
2020-04-29 01:33:04 +0000

    added function to test equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌❌❌Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([2, 4, 6], [2, 4 ,6]), true));
+console.log(assertEqual(eqArrays(['5', '10', '15'], ['5', '10', '15']), false));
