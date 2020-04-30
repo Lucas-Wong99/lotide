84ca807efa33afe57693943a3848b66075680122
Lucas Wong <lucaswong.ca@gmail.com>
2020-04-30 16:22:56 +0000

    Format and bug fixes

--- eqObjects.js
+++ eqObjects.js
@@ -26,7 +26,9 @@ const eqObjects = function(object1, object2) {
   }
   for (const key of keysArray1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     } else {
       if (object1[key] !== object2[key]) {
         return false;
@@ -44,15 +46,17 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
 
-console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const dl = { d: ["2", 3], c: "2" };
 
+console.log(assertEqual(eqObjects(ab, ba), true)); // => true
 
 console.log(assertEqual(eqObjects(ab, abc), false));
 
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
 console.log(assertEqual(eqObjects(cd, dc), true)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
+
+console.log(assertEqual(eqObjects(dl, dc), false)); // => false
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -21,12 +21,11 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+
 //we will be returning a string
 //use a for in loop top dtermine the key value pairs
 //if the the value === the iteration of the for in loop retrun the key
 //if there isnt a value retrun undefined
-
-
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
