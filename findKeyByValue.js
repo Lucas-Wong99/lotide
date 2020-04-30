2b98511f56e392b8ef53e00a83ea42d0b00281a2
Lucas Wong <lucaswong.ca@gmail.com>
2020-04-30 02:39:55 +0000

    added findKeyByValue file

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌❌❌Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+//we will be returning a string
+//use a for in loop top dtermine the key value pairs
+//if the the value === the iteration of the for in loop retrun the key
+//if there isnt a value retrun undefined
+
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
