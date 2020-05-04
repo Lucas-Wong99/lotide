9d672e7483d31f1a5eaefd7c57aee3cf6ef0c50c
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-04 19:40:36 +0000

    Refactored assertArrays equal and added its test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,10 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
 module.exports = assertArraysEqual;
